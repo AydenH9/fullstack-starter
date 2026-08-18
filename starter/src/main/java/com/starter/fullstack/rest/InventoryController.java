e5d2a3a7be701f75d0944d8cd9287a91d7addd78
Ayden Herring <aydenherring99@gmail.com>
2026-08-18 12:04:45 -0400

    Update Task 2
    Fixed import error to satisfy checkstyle specifications

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -3,13 +3,13 @@ package com.starter.fullstack.rest;
 import com.starter.fullstack.api.Inventory;
 import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Inventory Controller.
@@ -47,4 +47,3 @@ public class InventoryController {
     return this.inventoryDAO.create(inventory);
   }
 }
-
